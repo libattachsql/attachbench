96ef3814fbd945d7ad7584163d2bf08d42865c75
Andrew Hutchings <andrew@linuxjedi.co.uk>
2014-10-15 17:39:43 +0100

    Now works for a definition of works

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ attachbench
 ===========
 
 Benchmarking software for MySQL servers using libAttachSQL
+
+*NOT READY YET*
--- src/main.c
+++ src/main.c
@@ -19,6 +19,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <libattachsql-1.0/attachsql.h>
 
 const char *argp_program_version= "attachsql 0.1.0";
 const char *argp_bug_address= "<andrew@linuxjedi.co.uk>";
@@ -31,6 +34,8 @@ static struct argp_option options[]=
   {"port", 'o', "PORT", 0, "Port number (0 for socket)", 0},
   {"user", 'u', "USER", 0, "Username", 0},
   {"pass", 'p', "PASS", 0, "Password", 0},
+  {"db", 'd', "DATABASE", 0, "Database", 0},
+  {"queries", 'q', "QUERIES", 0, "Max queries", 0},
   {0}
 };
 
@@ -38,13 +43,25 @@ struct arguments
 {
   bool verbose;
   uint16_t connections;
-  char* host;
+  char *host;
   uint16_t port;
-  char* user;
-  char* pass;
+  char *user;
+  char *pass;
+  char *db;
+  uint64_t max_queries;
 };
 
+struct connect_data
+{
+  uint16_t con_no;
+  char query[40];
+  attachsql_connect_t *con;
+  struct arguments *arguments;
+};
+
+
 static error_t parse_opt(int key, char *arg, struct argp_state *state);
+void callbk(attachsql_connect_t *con, attachsql_events_t events, void *context, attachsql_error_t *error);
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
@@ -69,6 +86,12 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
     case 'p':
       arguments->pass= arg;
       break;
+    case 'd':
+      arguments->db= arg;
+      break;
+    case 'q':
+      arguments->max_queries= strtoull(arg, NULL, 10);
+      break;
     default:
       return ARGP_ERR_UNKNOWN;
       break;
@@ -79,19 +102,128 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 static char doc[] = "AttachBench, a benchmark for MySQL servers";
 static char args_doc[] = "";
 
+uint64_t query_done_counter= 0;
+uint64_t query_counter= 0;
+
 static struct argp argp = { options, parse_opt, args_doc, doc, NULL, NULL, NULL };
 
 int main(int argc, char *argv[])
 {
   struct arguments arguments;
+  const char *default_host= "localhost";
+  const char *default_user= "test";
+  const char *default_pass= "";
+
+  struct connect_data *con_data;
+  attachsql_group_t *group;
+  attachsql_error_t *error= NULL;
+  uint16_t con_counter;
 
   arguments.verbose= false;
   arguments.connections= 16;
-  arguments.host= NULL;
+  arguments.host= (char*)default_host;
   arguments.port= 3306;
-  arguments.user= NULL;
-  arguments.pass= NULL;
+  arguments.user= (char*)default_user;
+  arguments.pass= (char*)default_pass;
+  arguments.max_queries= 10000;
 
   argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+  con_data= malloc(sizeof(struct connect_data) * arguments.connections);
+  if (!con_data)
+  {
+    printf("Error allocating connection data");
+    return -1;
+  }
+
+  group= attachsql_group_create(NULL);
+  srand(time(NULL));
+
+  for (con_counter= 0; con_counter < arguments.connections; con_counter++)
+  {
+    /* Create connections and add them to the group */
+    con_data[con_counter].con= attachsql_connect_create(arguments.host, arguments.port, arguments.user, arguments.pass, arguments.db, NULL);
+    attachsql_connect_set_callback(con_data[con_counter].con, callbk, &con_data[con_counter]);
+    attachsql_group_add_connection(group, con_data[con_counter].con, &error);
+    con_data[con_counter].con_no= con_counter;
+    con_data[con_counter].arguments= &arguments;
+    attachsql_connect(con_data[con_counter].con, &error);
+    if (error)
+    {
+      printf("Setup error: %s", attachsql_error_message(error));
+      return -1;
+    }
+  }
+
+  while (query_done_counter < (arguments.max_queries + arguments.connections))
+  {
+    attachsql_group_run(group);
+  }
+  attachsql_group_destroy(group);
   return 0;
+}
+
+void callbk(attachsql_connect_t *con, attachsql_events_t events, void *context, attachsql_error_t *error)
+{
+  struct connect_data *con_data= (struct connect_data*)context;
+  uint16_t rand_id;
+
+  switch(events)
+  {
+    case ATTACHSQL_EVENT_CONNECTED:
+      if (con_data->arguments->verbose)
+      {
+        printf("Connection %d connected\n", con_data->con_no);
+      }
+    case ATTACHSQL_EVENT_EOF:
+      attachsql_query_close(con);
+      query_done_counter++;
+      if (query_counter < con_data->arguments->max_queries)
+      {
+        rand_id= rand() % 10000;
+        snprintf(con_data->query, 40, "SELECT * FROM sbtest WHERE id=%d\n", rand_id);
+        if (con_data->arguments->verbose)
+        {
+          printf("Sending query from %d: %s", con_data->con_no, con_data->query);
+        }
+        attachsql_query(con, strlen(con_data->query), con_data->query, 0, NULL, &error);
+        if (error)
+        {
+          printf("Error on con %d: %s", con_data->con_no, attachsql_error_message(error));
+          exit(-1);
+        }
+        query_counter++;
+      }
+      else
+      {
+        if (con_data->arguments->verbose)
+        {
+          printf("Connection %d finished", con_data->con_no);
+        }
+      }
+
+      break;
+    case ATTACHSQL_EVENT_ERROR:
+      printf("Error on con %d: %s", con_data->con_no, attachsql_error_message(error));
+      exit(-1);
+      break;
+    case ATTACHSQL_EVENT_ROW_READY:
+      attachsql_query_row_get(con, &error);
+      if (error)
+      {
+        printf("Error on con %d: %s", con_data->con_no, attachsql_error_message(error));
+        exit(-1);
+      }
+      if (con_data->arguments->verbose)
+      {
+        printf("Con: %d, got row\n", con_data->con_no);
+      }
+      attachsql_query_row_next(con);
+      break;
+    case ATTACHSQL_EVENT_NONE:
+    default:
+      /* should never happen */
+      abort();
+      break;
+  }
 }
